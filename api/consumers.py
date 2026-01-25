15a131029baf94ca05b8d0f54f8429f08a460b27
devnex730 <priyagupt32100@gmail.com>
2026-01-25 22:53:24 +0530

    last

--- api/consumers.py
+++ api/consumers.py
@@ -1,7 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 import json
-import google.genai as genai
+import google as genai
 import traceback
 import os
 
