5b68109a6665b37be6ac436f74f7ba22dfd56b45
devnex730 <priyagupt32100@gmail.com>
2026-01-25 23:11:19 +0530

    error fixing

--- api/ml.py
+++ api/ml.py
@@ -1,15 +1,22 @@
 from .models import JDmodel
+from asgiref.sync import sync_to_async
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-jd_corpus = list(JDmodel.objects.values_list("text", flat=True))
 
 Vectorizer = TfidfVectorizer(
     stop_words="english",
-    ngram_range=(1,2),
+    ngram_range=(1, 2),
     min_df=3,
     max_df=0.85,
     max_features=3000,
 )
-def train():
-    Vectorizer.fit(jd_corpus)
-    return f"model is traind on {len(jd_corpus)} JDs"
+
+def get_jd_corpus():
+    return list(JDmodel.objects.values_list("text", flat=True))
+
+async def get_jd_corpus_async():
+    return await sync_to_async(get_jd_corpus)()
+
+async def train():
+    corpus = await get_jd_corpus_async()
+    Vectorizer.fit(corpus)
+    return f"Model trained on {len(corpus)} JDs"
