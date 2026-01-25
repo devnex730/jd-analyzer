5b8204116252ae3e1dc11827bf0637e87c2688ef
devnex730 <priyagupt32100@gmail.com>
2026-01-25 22:56:22 +0530

    chats bot

--- api/consumers.py
+++ api/consumers.py
@@ -1,20 +1,17 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 import json
-import google as genai
+from google import genai
 import traceback
 import os
 
-genai.configure(api_key=os.environ.get("GOOGLE_GEMMA_API"))
-model = genai.GenerativeModel(
-    "gemma-3-27b-it",
-)
+client = genai.Client(api_key=os.environ.get("GOOGLE_GEMMA_API"))
+
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        self.chat = model.start_chat(history=[])
 
     async def receive(self, text_data):
         try:
@@ -27,7 +24,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 No long theory explanations.
                 """
                    +data.get("message"))
-            res = await sync_to_async(self.chat.send_message)(msg)
+            res = await sync_to_async(client.models.generate_content)(
+                model = "gemma-3-27b-it",
+                contents = msg)
             await self.send(json.dumps({"reply": res.text}))
 
         except Exception as e:
